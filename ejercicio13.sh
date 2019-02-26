7e19a34066828ca2191cf7d8f155d9bfcdf2b9dc
raul-ct <raul_ct_@hotmail.com>
2019-02-26 16:28:07 +0100

    mostrar permisos, y editarlos

--- ejercicio13.sh
+++ ejercicio13.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
-read -p "Pon el nombre del archivo: " arch
-
-perm=`ls -l $arch |cut -d" " -f1|tail -c 10`
-
-echo $perm
+echo "has usado $1"
+perm=`ls -l $1 |cut -d" " -f1|tail -c 10`
+echo "Estos son los permisos: $perm "
+test -w $1
+esc=`echo "$?"`
+if [ $esc != "0" ]
+then
+echo "Vamos a ponerle los permisos a $1"
+echo "Recuerda que va por letras, si no quieres ningun permiso dejalo vacío"
+echo "\n Empezamos con los permisos del propietario"
+read prop
+echo "Permisos de grupos"
+read grup
+echo "otros"
+read otros
+chmod -v u=$prop,g=$grup,o=$otros $1
+else
+echo "¿Los permisos estan bien?"
+fi
